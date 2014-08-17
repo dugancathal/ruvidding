307b7784345126f376eb75f47c21d5375a4933a9
TJ Taylor <dugancathal@gmail.com>
2014-08-17 15:56:12 -0600

    Add the vids#show page

--- lib/ruvidding/app.rb
+++ lib/ruvidding/app.rb
@@ -5,5 +5,15 @@ module Ruvidding
     get '/' do
       haml :index
     end
+
+    post '/start' do
+      return status 400 unless params['vid-name']
+      redirect to("/vids/#{params['vid-name']}")
+    end
+
+    get '/vids/:vid_name' do
+      @vid_name = params['vid_name']
+      haml :vid
+    end
   end
 end
--- spec/functional/app_spec.rb
+++ spec/functional/app_spec.rb
@@ -7,10 +7,35 @@ RSpec.describe Ruvidding::App do
     Ruvidding::App
   end
 
-  describe '/' do
+  describe 'GET /' do
     it 'has an element for the local-video' do
       get '/'
       expect(last_response.body).to match(/<div id='local-video'>/)
+    end
+  end
+
+  describe 'POST /start' do
+    it 'expects a vid-name param' do
+      post '/start'
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'redirects to /vids/:vid-name' do
+      post '/start', 'vid-name' => 'mangy-baboon'
+      follow_redirect!
+      expect(last_request.path).to eq '/vids/mangy-baboon'
+    end
+  end
+
+  describe 'GET /vids/:vid-name' do
+    it 'has an element for the local-video, too' do
+      get '/vids/testing'
+      expect(last_response.body).to match(/<div id='local-video'>/)
+    end
+
+    it 'displays the vid name' do
+      get '/vids/testing'
+      expect(last_response.body).to match(/testing/)
     end
   end
 end
