05550a5b3dfebe5f977acf4852aad92c91528928
TJ Taylor <dugancathal@gmail.com>
2015-08-30 18:30:29 -0600

    Add TURN/STUN support

--- lib/ruvidding/app.rb
+++ lib/ruvidding/app.rb
@@ -1,7 +1,11 @@
 require 'sinatra/base'
+require 'httparty'
+require 'json'
 
 module Ruvidding
   class App < Sinatra::Base
+    XIRSYS_URL='https://service.xirsys.com/ice'
+
     get '/' do
       haml :index
     end
@@ -14,6 +18,21 @@ module Ruvidding
     get '/vids/:vid_name' do
       @vid_name = params['vid_name']
       haml :vid
+    end
+
+    get '/ice' do
+      result = JSON.parse(
+        HTTParty.get(XIRSYS_URL, query: {
+          ident: ENV['XIRSYS_IDENT'],
+          secret: ENV['XIRSYS_TOKEN'],
+          domain: 'ruvidding.me',
+          application: 'ruviddingme',
+          room: 'ruviddingme',
+          secure: 1
+        }).body
+      )
+      content_type 'application/json'
+      Array((result['d'] || {})['iceServers']).to_json
     end
   end
 end
--- ruvidding.gemspec
+++ ruvidding.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'thin', '~> 1.6.2'
   spec.add_dependency 'faye', '~> 1.0.3'
   spec.add_dependency 'haml', '~> 4.0.5'
+  spec.add_dependency 'httparty'
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
